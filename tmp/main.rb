03c01c3219e4e5d6a9ed3a98e08c2db34fc8a47c
Fredrik Svard <fredrik.svard@gmail.com>
2018-03-18 11:23:29 +0100

    more on this

---
+++ tmp/main.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby 
+require 'rubygems'
+require 'rest-client'
+
+#gem_name = ARGV[0]
+gem_name = 'cucumber'
+
+raise ArgumentError.new("gem name missing") if gem_name.nil?
+
+response = RestClient.get("https://rubygems.org/gems/#{gem_name}")
+
+puts RUBY_VERSION
+
+puts response.inspect
+
+if response.code == 200
+  $stdout.puts 'Name  available.' 
+else 
+  $stdout.puts 'Name not available.' 
+end
