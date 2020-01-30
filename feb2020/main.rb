1dc7af31f9a89066b8a17fdb312cefee73afc59a
Fredrik Svard <fredrik.svard@gmail.com>
2020-01-30 21:52:46 +0100

    this will work

---
+++ feb2020/main.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+require 'is_even'
+
+puts "hej hej!#{IsEven::well_is_it?(2)}"
---
+++ feb2020/run_docker.sh
@@ -0,0 +1,3 @@
+
+sudo docker build -t my-ruby-app .
+sudo docker run -it --name my-running-script my-ruby-app
